ecdb13862855ae02286ef57a150917b4f060b6fe
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-11-04 08:25:11 +0700

    create several components

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI- Powered Prompt</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open source AI prompting tool for modern world to discover, create and share creative prompts
+      </p>
 
-export default Home
+      {/* feed */}
+    </section>
+  );
+};
+
+export default Home;
